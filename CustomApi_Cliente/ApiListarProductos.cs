461899ad0ae6f4a8554ab6fa5233a670a747165b
Ignacio Korenhof <ig.korenhof@alumnos.duoc.cl>
2023-08-03 12:01:38 -0400

    Refactor: Custom APIs Cliente

---
+++ CustomApi_Cliente/ApiListarProductos.cs
@@ -0,0 +1,65 @@
+﻿using CustomApi_Cliente;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomApi_Cliente
+{
+    public class ApiListarProductos : Services
+    {
+        public override void Execute(IServiceProvider serviceProvider)
+        {
+            SetServices(serviceProvider);
+
+            // Parametros de entrada de la API
+            Guid clienteGuid = (Guid)Context.InputParameters["id"];
+
+            if (clienteGuid != Guid.Empty)
+            {
+                try
+                {
+                    // Obtener todos los productos
+                    ColumnSet cs = new ColumnSet("cr8e5_productoaofrecerid", "cr8e5_name", "cr8e5_fechavigencia");
+                    EntityCollection productos = MultipleQuery("cr8e5_productoaofrecer", cs, Service);
+
+                    // Obtener los productos que se le han ofrecido al cliente
+                    cs = new ColumnSet("cr8e5_productoaofrecer", "cr8e5_estadooferta");
+                    FilterExpression filterExpression = new FilterExpression();
+                    filterExpression.AddCondition("cr8e5_contact", ConditionOperator.Equal, clienteGuid);
+
+                    filterExpression.AddCondition("cr8e5_estadooferta", ConditionOperator.Between, 0, 1);
+                    EntityCollection clienteProductoAux = MultipleQuery("cr8e5_clienteproducto", cs, Service, filterExpression);
+
+                    var productosOfrecidos = clienteProductoAux.Entities.Select(x => (EntityReference)x.Attributes["cr8e5_productoaofrecer"]);
+
+                    EntityCollection ofertasCliente = new EntityCollection();
+
+                    foreach (var item in productos.Entities)
+                    {
+                        bool existe = productosOfrecidos.Any(x => x.Id.Equals(item.Id));
+
+                        if (!existe)
+                        {
+                            ofertasCliente.Entities.Add(item);
+                        }
+                    }
+
+                    Context.OutputParameters["productos"] = ofertasCliente;
+                    Context.OutputParameters["idcliente"] = clienteGuid;
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+        }
+    }
+}
---
+++ CustomApi_Cliente/Services.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomApi_Cliente
+{
+    public abstract class Services : IPlugin
+    {
+        private ITracingService tracingService;
+        private IPluginExecutionContext context;
+        private IOrganizationServiceFactory serviceFactory;
+        private IOrganizationService service;
+
+        public ITracingService TracingService { get => tracingService; set => tracingService = value; }
+        public IPluginExecutionContext Context { get => context; set => context = value; }
+        public IOrganizationServiceFactory ServiceFactory { get => serviceFactory; set => serviceFactory = value; }
+        public IOrganizationService Service { get => service; set => service = value; }
+
+        public abstract void Execute(IServiceProvider serviceProvider);
+        public void SetServices(IServiceProvider serviceProvider)
+        {
+            TracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            Context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            ServiceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            Service = ServiceFactory.CreateOrganizationService(Context.UserId);
+        }
+
+        public EntityCollection MultipleQuery(string entityName, ColumnSet columnSet, IOrganizationService Service, FilterExpression filterExpression = null, List<OrderExpression> orderExpressions = null)
+        {
+            QueryExpression query = new QueryExpression(entityName)
+            {
+                ColumnSet = columnSet,
+            };
+            if (filterExpression != null) { query.Criteria = filterExpression; }
+            if (orderExpressions != null)
+            {
+                foreach (var item in orderExpressions)
+                {
+                    query.AddOrder(item.AttributeName, item.OrderType);
+                    query.AddOrder(item.AttributeName, item.OrderType);
+                }
+
+            }
+            EntityCollection entityCollection = Service.RetrieveMultiple(query);
+            return entityCollection;
+        }
+    }
+}
